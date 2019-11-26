b6c90c0cc31ac1d8475b18364074f6ae3eb1ff7f
amoghvnaik <amoghvnaik@yahoo.com>
2019-11-26 16:07:57 +0000

    commit

--- test/test_something.py
+++ test/test_something.py
@@ -1,9 +1,9 @@
 import pytest
-from code import abcd
+from code import something
 
 
 def test_factorial_5():
-    assert abcd.factorial([5])==120
+    assert something.factorial([5])==120
 
 def test_factorial_6():
-    assert abcd.factorial([6])==720
+    assert something.factorial([6])==720
