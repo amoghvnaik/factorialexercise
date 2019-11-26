9b4bada0b68a9e9ca4a75bd85401d3209843b531
amoghvnaik <amoghvnaik@yahoo.com>
2019-11-26 15:28:36 +0000

    commit

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@ from code import code
 
 
 def test_factorial_5():
-    assert factorial.factorial([5])==120
+    assert code.factorial([5])==120
 
 def test_factorial_6():
-    assert factorial.factorial([6])==720
+    assert code.factorial([6])==720
